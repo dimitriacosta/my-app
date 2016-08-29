ef316c525cf116035590e8d2cfdea2860c486205
Dimitri Acosta <acosta.dimitri@gmail.com>
2016-08-29 11:43:14 -0600

    Change welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Git</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Bienvenidos al curso de git en Styde.
                 </div>
             </div>
         </div>
